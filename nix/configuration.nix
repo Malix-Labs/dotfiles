001eb4b5f788dbf5a05824ba2adb6f3b23e4e548
Malix <alixbrunetcontact@gmail.com>
2025-09-21 18:48:55 +0200

    fix: substituers and trusted public keys

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -15,8 +15,22 @@
       "nix-command"
       "flakes"
     ];
-    extra-substituters = [ "https://install.determinate.systems" ];
-    extra-trusted-public-keys = [ "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM=" ];
+    substituters = [
+      "https://cache.nixos.org/" # don't know if this one is included by default already
+      "https://nix-community.cachix.org/"
+
+      "https://hyprland.cachix.org/"
+
+      "https://install.determinate.systems"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" # don't know if this one is included by default already
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+
+      "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
+    ];
   };
 
   boot = {
