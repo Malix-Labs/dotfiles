ac90ee7be8547ea2202faae75701f968cb48d597
Malix <alixbrunetcontact@gmail.com>
2025-09-23 15:53:47 +0200

    enhance: rebuild.sh

--- nix/rebuild.sh
+++ nix/rebuild.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-cd /etc/nixos/
+cd /etc/nixos/ || return
 
 nix flake update
 
-nixos-rebuild switch \
-	--option extra-substituters https://install.determinate.systems \
-	--option extra-trusted-public-keys cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM= \
+nixos-rebuild switch
