c69b093f7387981aa78b87e1f9dfa01606040020
Malix <alixbrunetcontact@gmail.com>
2025-10-13 17:18:51 +0200

    enhance(nix): nixpkgs inputs

--- nix/flake.nix
+++ nix/flake.nix
@@ -3,8 +3,8 @@
   inputs = {
     determinate.url = "github:DeterminateSystems/determinate";
 
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.05";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "https://channels.nixos.org/nixos-25.05/nixexprs.tar.xz";
+    nixpkgs-unstable.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
 
     nixos-hardware.url = "github:NixOS/nixos-hardware";
 
