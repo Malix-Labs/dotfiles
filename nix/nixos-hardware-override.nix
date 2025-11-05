73769327ebcec516b69d8ac995a36c0fa85a4426
Malix <alixbrunetcontact@gmail.com>
2025-11-05 19:17:34 +0100

    update(nix): remove nvidia workaround

--- nix/nixos-hardware-override.nix
+++ nix/nixos-hardware-override.nix
@@ -10,16 +10,6 @@
 {
   hardware.nvidia.prime.amdgpuBusId = "PCI:5:0:0"; # See https://github.com/NixOS/nixos-hardware/issues/1388
 
-  # Required until NixOS 25.11 is released OR https://github.com/NixOS/nixpkgs/issues/429624 gets fixed
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-    version = "580.95.05";
-    sha256_64bit = "sha256-hJ7w746EK5gGss3p8RwTA9VPGpp2lGfk5dlhsv4Rgqc=";
-    sha256_aarch64 = "sha256-zLRCbpiik2fGDa+d80wqV3ZV1U1b4lRjzNQJsLLlICk=";
-    openSha256 = "sha256-RFwDGQOi9jVngVONCOB5m/IYKZIeGEle7h0+0yGnBEI=";
-    settingsSha256 = "sha256-F2wmUEaRrpR1Vz0TQSwVK4Fv13f3J9NJLtBe4UP2f14=";
-    persistencedSha256 = "sha256-QCwxXQfG/Pa7jSTBB0xD3lsIofcerAWWAHKvWjWGQtg=";
-  };
-
   # Add Lenovo Legion kernel module and userspace utility
   boot.extraModulePackages = [ config.boot.kernelPackages.lenovo-legion-module ];
   environment.systemPackages = [
