82f2e07d42cb0977a5cd152a1bfce97913454cb5
Malix <alixbrunetcontact@gmail.com>
2025-11-30 20:46:04 +0100

    feat(nix): `virtualisation.podman.dockerSocket.enable`

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -178,6 +178,7 @@
     podman = {
       enable = true;
       dockerCompat = true;
+      dockerSocket.enable = true;
       defaultNetwork.settings.dns_enabled = true; # Required for containers under podman-compose to be able to talk to each other. See https://wiki.nixos.org/wiki/Podman
     };
     waydroid.enable = true;
