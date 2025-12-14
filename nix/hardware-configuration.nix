730eb57c6f871f5c4e0f4b1b7e377de479bc4c9c
Malix <alixbrunetcontact@gmail.com>
2025-12-14 18:28:39 +0100

    update(nix): generate-config

--- nix/hardware-configuration.nix
+++ nix/hardware-configuration.nix
@@ -28,14 +28,6 @@
     [ { device = "/dev/disk/by-uuid/bc2e19a0-d1ee-4fa5-abb2-3e8f3d8a7792"; }
     ];
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp4s0.useDHCP = lib.mkDefault true;
-
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
