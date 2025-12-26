b78737cfbe5fe49689bdf11ccdc5338a1c615007
Malix - Alix Brunet <alixbrunetcontact@gmail.com>
2025-12-26 14:43:48 +0100

    Revert "hotfix(nix): blacklist nova_core"
    This reverts commit ae397bc95db82edde635aa01cf5e1f5a8f223b22.

--- nix/nixos-hardware-override.nix
+++ nix/nixos-hardware-override.nix
@@ -8,8 +8,6 @@
   ...
 }:
 {
-  boot.blacklistedKernelModules = [ "nova_core" ]; # Required until https://github.com/NixOS/nixpkgs/issues/473350 is fixed
-
   hardware.nvidia.prime.amdgpuBusId = "PCI:5:0:0"; # See https://github.com/NixOS/nixos-hardware/issues/1388
 
   # Add Lenovo Legion kernel module and userspace utility
