43d5709ddb052a97b29064d73e2bf21b210822a1
Malix - Alix Brunet <alixbrunetcontact@gmail.com>
2026-02-03 17:18:23 +0100

    enhance(nix): steam

--- nix/gaming.nix
+++ nix/gaming.nix
@@ -1,9 +1,23 @@
 {
+  pkgs,
+  ...
+}:
+
+{
   programs = {
     steam = {
       enable = true;
       gamescopeSession.enable = true;
+      protontricks.enable = true;
+      extest.enable = true;
+      extraCompatPackages = [ pkgs.proton-ge-bin ];
     };
-    gamescope.enable = true;
+
+    gamescope = {
+      enable = true; # implicitely enabled by `steam.gamescopeSession.enable`, but required to set other options
+      capSysNice = true;
+    };
+
+    gamemode.enable = true;
   };
 }
