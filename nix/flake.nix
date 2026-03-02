54766c344e48387363aa8a909e570fee6861e290
Malix - Alix Brunet <alixbrunetcontact@gmail.com>
2026-03-02 23:44:15 +0100

    deprecate(nix): default susbtituers

--- nix/flake.nix
+++ nix/flake.nix
@@ -41,7 +41,6 @@
 
   nixConfig = {
     extra-substituters = [
-      "https://cache.nixos.org/" # don't know if this one is included by default already
       "https://nix-community.cachix.org/"
 
       "https://hyprland.cachix.org/"
@@ -51,7 +50,6 @@
       "https://zed-industries.cachix.org"
     ];
     extra-trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" # don't know if this one is included by default already
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
 
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
