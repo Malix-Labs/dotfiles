62cdeb3a64891ad749d1de3c82649cacee8d9d63
Malix - Alix Brunet <alixbrunetcontact@gmail.com>
2026-03-21 17:05:04 +0100

    update(nix): nixfmt-rfc-style to nixfmt

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -152,7 +152,7 @@
       # clipboard-jh # broken for Wayland, see https://github.com/Slackadays/Clipboard/issues/171
 
       nixd
-      nixfmt-rfc-style
+      nixfmt
       fh.packages.${pkgs.stdenv.hostPlatform.system}.default
       mcp-nixos.packages.${pkgs.stdenv.hostPlatform.system}.default
 
