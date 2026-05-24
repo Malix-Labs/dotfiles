3746b784bc1a0e9b8c0382795a8fb257fc74230b
Malix - Alix Brunet <alixbrunetcontact@gmail.com>
2026-05-24 13:55:47 +0200

    hotfix(gaming): steam disable gamescopeSession

--- nix/gaming.nix
+++ nix/gaming.nix
@@ -19,7 +19,7 @@ in
   programs = {
     steam = {
       enable = true;
-      gamescopeSession.enable = true;
+      # gamescopeSession.enable = true; # hotfix for https://github.com/NixOS/nixpkgs/issues/523427
       protontricks.enable = true;
       extest.enable = true;
       extraCompatPackages = steamCompatTools;
