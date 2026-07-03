7e11811ccc4ad7253efe0d244a158bee129f7867
Malix - Alix Brunet <alixbrunetcontact@gmail.com>
2026-07-03 04:45:45 +0200

    nix: rely on upstream sane default variant package

--- nix/home.nix
+++ nix/home.nix
@@ -66,11 +66,7 @@ in
       flake = "${config.home.homeDirectory}/${dotfilesDirectory}/nix";
     };
 
-    nix-index = {
-      enable = true;
-      package = nix-index-database.packages.${pkgs.stdenv.hostPlatform.system}.nix-index-with-small-db;
-    };
-
+    nix-index.enable = true;
     nix-index-database.comma.enable = true;
 
     nushell = {
