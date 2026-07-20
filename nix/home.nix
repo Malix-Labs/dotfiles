b5856c34f7ca9aadc8c669dda19c17816df0b52f
Malix - Alix Brunet <alixbrunetcontact@gmail.com>
2026-07-21 01:44:19 +0200

    feat(hm): git alias retrigger

--- nix/home.nix
+++ nix/home.nix
@@ -168,6 +168,7 @@ in
           "uncommit" = "reset --soft HEAD~1";
           "recommit" = "commit -C ORIG_HEAD";
           "pf" = "push --force-with-lease";
+          "retrigger" = "!git commit --amend --no-edit && git push --force-with-lease";
           "remote-swap" =
             "!f() { git remote rename origin upstream && git remote add origin \"$1\" && git fetch origin; }; f";
           "remote-unfork" =
