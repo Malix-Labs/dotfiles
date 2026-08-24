988c282871c193eb9a7c80d1ede53b8e96e7af2e
Malix - Alix Brunet <alixbrunetcontact@gmail.com>
2026-08-24 13:13:48 +0200

    chore(flake.lock): bump nixos-winpe to v0.4.1

--- nix/nixos-hardware-override.nix
+++ nix/nixos-hardware-override.nix
@@ -30,4 +30,10 @@
     lenovo-legion
     l5p-keyboard-rgb.packages.${stdenv.hostPlatform.system}.default
   ];
+
+  # Udev rules for Lenovo Legion RGB Keyboard (L5P-Keyboard-RGB)
+  # services.udev.extraRules = ''
+  #   SUBSYSTEM=="usb", ATTR{idVendor}=="048d", MODE="0666"
+  #   KERNEL=="hidraw*", ATTRS{idVendor}=="048d", MODE="0666"
+  # '';
 }
